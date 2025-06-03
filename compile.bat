0d432b766e257ff89134a80ceb0acc7283252c29
kitodoescode <156444563+kitodoescode@users.noreply.github.com>
2025-06-03 10:03:30 +0530

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-electron-packager . ArcadiaUI --platform=win32 --arch=x64 --asar.compression=zstd --overwrite
+npx electron-packager . ArcadiaUI --platform=win32 --arch=x64 --asar.compression=zstd --overwrite
 pause
